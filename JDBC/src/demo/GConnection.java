00fe767358eb723063313075997c4a79bf57ad7c
hjdeng2 <hjdeng@amarsoft.com>
2017-10-09 18:20:53 +0800

    20171009

--- JDBC/src/demo/GConnection.java
+++ JDBC/src/demo/GConnection.java
@@ -10,33 +10,39 @@ public class GConnection {
 	private static String url;
 	private static String username;
 	private static String password;
-	
+
 	public static String getDriverClass() {
 		return driverClass;
 	}
+
 	public static void setDriverClass(String driverClass) {
 		GConnection.driverClass = driverClass;
 	}
-	
+
 	public static String getUrl() {
 		return url;
 	}
+
 	public static void setUrl(String url) {
 		GConnection.url = url;
 	}
+
 	public static String getUsername() {
 		return username;
 	}
+
 	public static void setUsername(String username) {
 		GConnection.username = username;
 	}
+
 	public static String getPassword() {
 		return password;
 	}
+
 	public static void setPassword(String password) {
 		GConnection.password = password;
 	}
-	
+
 	/**
 	 * 
 	 * @param url
@@ -44,35 +50,31 @@ public class GConnection {
 	 * @param password
 	 * @throws Exception
 	 */
-	public static Connection getConnectrion() throws Exception{
-		//读取配置文件中配置的数据库数据
+	public static Connection getConnection() throws Exception {
+		// 读取配置文件中配置的数据库数据
 		Properties pro = new Properties();
-		FileInputStream  oFile = new FileInputStream("bin/jdbc.properties");
+		FileInputStream oFile = new FileInputStream("bin/jdbc.properties");
 		pro.load(oFile);
 		oFile.close();
-		String classDriver = null ;
+		String classDriver = null;
 		String url = null;
-		String userName= null;
+		String userName = null;
 		String password = null;
-/*		classDriver = pro.getProperty("classDriver");
-		url = pro.getProperty("url");
-		userName = pro.getProperty("userName");
-		password = pro.getProperty("password");*/
-		
-		
-		//注册驱动
+		/*
+		 * classDriver = pro.getProperty("classDriver"); url =
+		 * pro.getProperty("url"); userName = pro.getProperty("userName");
+		 * password = pro.getProperty("password");
+		 */
+
+		// 注册驱动
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-	    //Connection conn = DriverManager.getConnection(url, userName, password);
-	    Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.234.1:1521:orcl", "cjcbhb", "cjcbhb");
-	    return conn;
-		
+		// Connection conn = DriverManager.getConnection(url, userName,
+		// password);
+		Connection conn = DriverManager
+				.getConnection("jdbc:oracle:thin:@192.168.234.1:1521:orcl",
+						"cjcbhb", "cjcbhb");
+		return conn;
+
 	}
-	
-	
-	
-	
-	
-	public static void main(String[] args) throws Exception {
-		GConnection.getConnectrion();
-	}
+
 }
---
+++ JDBC/src/demo/StatementDemo.java
@@ -0,0 +1,52 @@
+package demo;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class StatementDemo {
+
+	@Test
+	public void selTest() throws Exception {
+		Connection conn = GConnection.getConnection();
+		Statement stmt = conn.createStatement();
+		String sql = "select  username,belongorg from user_info where userid='901003'";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			String username = rs.getString("username");
+			String belongOrg = rs.getString("belongorg");
+			String orgname = null;
+			if (belongOrg == null | belongOrg == "")
+				belongOrg = null;
+			String sql1 = "select  orgname from org_info where orgid="
+					+ belongOrg;
+			ResultSet rs2 = stmt.executeQuery(sql1);
+			while (rs2.next()) {
+				orgname = rs2.getString("orgname");
+			}
+			System.out.println("用户名：" + username + "，隶属机构：" + orgname);
+			rs2.close();
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
+	
+	 @Test
+	  public  void DMLtest() throws Exception{
+		 Connection conn = GConnection.getConnection();
+		 Statement stmt = conn.createStatement();
+		 String sql = "create table student(id number(20),name varchar2(20),gender varchar2(20))";
+		 String sql2 = "drop table student";
+		 stmt.execute(sql2);
+		 Boolean flag =  stmt.execute(sql);
+		 System.out.println(flag);
+		 if(flag==true){
+			 System.out.println("student表创建成功!");
+		 }
+		 stmt.close();
+		 conn.close();
+	 }
+}
